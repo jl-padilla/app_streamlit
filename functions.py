8a5c9c58183ffe84362f7ddc4d3b40eb7e7eabae
jl-padilla <joseluis_padilla@hotmail.es>
2025-06-30 13:13:16 +0200

    250630

--- functions.py
+++ functions.py
@@ -128,7 +128,7 @@ def map(df):
 import streamlit as st
 
 def resultados(df):
-    st.expander("Datos resultado:"):
+    st.expander("Datos resultado:")
     st.dataframe(df)
 
 #def resultados(df):
